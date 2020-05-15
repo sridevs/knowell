47f5766cfccda08653199cfd5dab69e4b92e5515
Sridev S <sridevs@thoughtworks.com>
2020-05-15 09:02:18 +0530

    1 [Sridev] | upgraded knex version from 0.13 to 0.14.6

--- migrations/utils/runner.js
+++ migrations/utils/runner.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
+const fs = require('fs');
 
 module.exports = function (sqlFile) {
-    return function (knex, Promise) {
-        var sql = fs.readFileSync(sqlFile).toString();
-        return Promise.join(
-            knex.raw(sql)
-        );
-    }
+	return function (knex, Promise) {
+		const sql = fs.readFileSync(sqlFile).toString();
+		return Promise.join(
+			knex.raw(sql)
+		);
+	};
 };
