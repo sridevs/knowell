1b8c87a1e09d86b9d0ebabc666d8fb95df34216d
Sridev S <sridevs@thoughtworks.com>
2020-05-16 07:54:46 +0530

    1 [Sridev] | Switching from circle/mysql to circle/node:11 image

--- .setup.sh
+++ .setup.sh
@@ -5,26 +5,26 @@ CREATE_LIBRARY_TEST="CREATE DATABASE library_test /*\!40100 DEFAULT CHARACTER SE
 CREATE_USER="CREATE USER knowell@localhost IDENTIFIED BY '';"
 GRANT_PRIVILEGES_LIB_DEV="GRANT ALL PRIVILEGES ON library_dev.* TO 'knowell'@'localhost';"
 GRANT_PRIVILEGES_LIB_TEST="GRANT ALL PRIVILEGES ON library_test.* TO 'knowell'@'localhost';"
-ALTER_AUTH_METHOD="ALTER USER 'knowell'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+ALTER_AUTH_METHOD="ALTER USER 'knowell'@'localhost' IDENTIFIED WITH sudo mysql_native_password BY ''"
 FLUSH_PRIVILEGES="FLUSH PRIVILEGES;"
 # If /root/.my.cnf exists then it won't ask for root password
 if [ -f /root/.my.cnf ]; then
 
-  mysql -e "${CREATE_LIBRARY_DEV}"
-  mysql -e "${CREATE_LIBRARY_TEST}"
-  mysql -e "${CREATE_USER}"
-  mysql -e "${GRANT_PRIVILEGES_LIB_DEV}"
-  mysql -e "${GRANT_PRIVILEGES_LIB_TEST}"
-  mysql -e "${FLUSH_PRIVILEGES}"
+  sudo mysql -e "${CREATE_LIBRARY_DEV}"
+  sudo mysql -e "${CREATE_LIBRARY_TEST}"
+  sudo mysql -e "${CREATE_USER}"
+  sudo mysql -e "${GRANT_PRIVILEGES_LIB_DEV}"
+  sudo mysql -e "${GRANT_PRIVILEGES_LIB_TEST}"
+  sudo mysql -e "${FLUSH_PRIVILEGES}"
 
 # If /root/.my.cnf doesn't exist then it'll ask for root password
 else
 
-  mysql --user=root --password=password -e "${CREATE_LIBRARY_DEV}"
-  mysql --user=root --password=password -e "${CREATE_LIBRARY_TEST}"
-  mysql --user=root --password=password -e "${CREATE_USER}"
-  mysql --user=root --password=password -e "${GRANT_PRIVILEGES_LIB_DEV}"
-  mysql --user=root --password=password -e "${GRANT_PRIVILEGES_LIB_TEST}"
-  mysql --user=root --password=password -e "${ALTER_AUTH_METHOD}"
-  mysql --user=root --password=password -e "${FLUSH_PRIVILEGES}"
+  sudo mysql --user=root --password=password -e "${CREATE_LIBRARY_DEV}"
+  sudo mysql --user=root --password=password -e "${CREATE_LIBRARY_TEST}"
+  sudo mysql --user=root --password=password -e "${CREATE_USER}"
+  sudo mysql --user=root --password=password -e "${GRANT_PRIVILEGES_LIB_DEV}"
+  sudo mysql --user=root --password=password -e "${GRANT_PRIVILEGES_LIB_TEST}"
+  sudo mysql --user=root --password=password -e "${ALTER_AUTH_METHOD}"
+  sudo mysql --user=root --password=password -e "${FLUSH_PRIVILEGES}"
 fi
